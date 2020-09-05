314889abca03db7069384c3898e941c1a1ae1b0d
aladdinmateo <52956434+aladdinmateo@users.noreply.github.com>
2020-09-05 17:32:27 +0200

    svg-sprite-update II

--- maNode/Project/Gulp/myTasks/sprite.js
+++ maNode/Project/Gulp/myTasks/sprite.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    sprite = require('gulp-svg-sprite');
+    sprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename');
 
 var config = {
       mode: {
         css: {
+          sprite: 'sprite.svg',
           render: {
             css: {
               template: 'Gulp/template/sprite.css'
@@ -18,3 +20,16 @@ gulp.task('createSprite', function() {
   .pipe(sprite(config))
   .pipe(gulp.dest('tt/Css/Sprite/'));
 });
+
+gulp.task('copySpriteLocate', ['createSprite'], function() {
+  return gulp.src('tt/Css/Sprite/css/**/*.svg')
+   .pipe(gulp.dest('tt/assets/images/sprite'))
+});
+
+gulp.task('copySpriteCss', ['createSprite'], function() {
+  return gulp.src('tt/Css/Sprite/css/*.css')
+  .pipe(rename('_sprite.css'))
+  .pipe(gulp.dest('tt/Css/again/newStyle'));
+});
+
+gulp.task('icon', ['createSprite', 'copySpriteLocate', 'copySpriteCss']);
